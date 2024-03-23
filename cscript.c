bfcbed221b95a1e0008dd6d0328515bf325a4daf
Lukas Sandforth <lsandfo@gmail.com>
2024-03-23 15:59:17 +0100

    Add save function -s
    Adding function to save the last executed cscript as binary to the
    actually location as 'executable_cscript.elf'.

--- cscript.c
+++ cscript.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define MAX_LENGTH 50
 
@@ -12,7 +12,7 @@ void debug();
 void usage(char *argv);
 int test_file();
 void run_script(char *script);
-
+int save_data();
 
 // Configurations
 char COMPILER[MAX_LENGTH] = "gcc",
@@ -25,124 +25,174 @@ const bool CREATE_MAIN = true;
 char *empty_data = "\n";
 
 // Main
-int main(int argc, char *argv[]){
-		// Check for environment variable 'EDITOR' and replace it with the Internal, if not empty
-		ENV_EDITOR = getenv("EDITOR");
-		if (ENV_EDITOR != NULL)
-				strcpy(EDITOR, ENV_EDITOR);
+int main(int argc, char *argv[])
+{
+	// Check for environment variable 'EDITOR' and replace it with the Internal, if not empty
+	ENV_EDITOR = getenv("EDITOR");
+	if (ENV_EDITOR != NULL)
+		strcpy(EDITOR, ENV_EDITOR);
 
 	// Check for given arguments or file
-		if (argc == 1){
-				edit();
-				return EXIT_SUCCESS;
-		}
-		else if ((strcmp(argv[1], "-h" )) == 0)
-				usage(argv[0]);
-		else if ((strcmp(argv[1], "-r" )) == 0)
-				edit();
-		else if ((strcmp(argv[1], "-n" )) == 0){
-				build_frame(empty_data);
-				edit();
-		}
-		else if ((strcmp(argv[1], "-d" )) == 0)
-				debug();
-		else
-				run_script(argv[1]);
+	if (argc == 1)
+	{
+		edit();
+		return EXIT_SUCCESS;
+	}
+	else if ((strcmp(argv[1], "-h")) == 0)
+		usage(argv[0]);
+	else if ((strcmp(argv[1], "-r")) == 0)
+		edit();
+	else if ((strcmp(argv[1], "-n")) == 0)
+	{
+		build_frame(empty_data);
+		edit();
+	}
+	else if ((strcmp(argv[1], "-d")) == 0)
+		debug();
+	else if ((strcmp(argv[1], "-s")) == 0)
+	{
+		if (save_data() != 0)
+			return EXIT_FAILURE;
+	}
+	else
+		run_script(argv[1]);
 
-
-		return EXIT_SUCCESS;		
+	return EXIT_SUCCESS;
 }
 
-
 // The help message
-void usage(char *argv){
-		printf("Usage: %s [-rndh]\n"
-	"-r: Resume last script\n"
-	"-n: Create a new Script and clear cache\n"
-	"-d: Run last Script with a debugger\n"
-	"-h: Print this help message\n"
-	"\n"
-	"If a file is given as argument, it's conntent will wrapped inside of a default\n"
-	"main function, including stdio.h an stdlib.h, compile it and run it.\n"
-	"Else if no argument is given, the last Script is re opend.\n"
-	"In case no file exist to resume a new one is created.\n"
-	"\n"
-	"Please keep in mind that the default location for the written code is\n"
-	"in /dev/shm, which means the code exists only temporary inside of you memory.\n"
-	"By default, without a custom location, you code WON'T SURVIVE A REBOOT!\n"
-	"In case you system got no /dev/shm, the /tmp directory will be used, to store you code.\n", argv);
+void usage(char *argv)
+{
+	printf("Usage: %s [-rndh]\n"
+		   "-r: Resume last script\n"
+		   "-n: Create a new Script and clear cache\n"
+		   "-d: Run last Script with a debugger\n"
+		   "-h: Print this help message\n"
+		   "\n"
+		   "If a file is given as argument, it's conntent will wrapped inside of a default\n"
+		   "main function, including stdio.h an stdlib.h, compile it and run it.\n"
+		   "Else if no argument is given, the last Script is re opend.\n"
+		   "In case no file exist to resume a new one is created.\n"
+		   "\n"
+		   "Please keep in mind that the default location for the written code is\n"
+		   "in /dev/shm, which means the code exists only temporary inside of you memory.\n"
+		   "By default, without a custom location, you code WON'T SURVIVE A REBOOT!\n"
+		   "In case you system got no /dev/shm, the /tmp directory will be used, to store you code.\n",
+		   argv);
 }
 
 // The function to edit the file
-void edit(){	
-		int test = test_file();
-		if (test)
-				build_frame(empty_data);
-		system(strcat(EDITOR, " /dev/shm/cscript.c"));
-		system(strcat(COMPILER, " -g -o /dev/shm/cscript.elf /dev/shm/cscript.c"));
-		system("/dev/shm/cscript.elf");
+void edit()
+{
+	int test = test_file();
+	if (test)
+		build_frame(empty_data);
+	system(strcat(EDITOR, " /dev/shm/cscript.c"));
+	system(strcat(COMPILER, " -g -o /dev/shm/cscript.elf /dev/shm/cscript.c"));
+	system("/dev/shm/cscript.elf");
 }
 
 // Create Main function frame
-void build_frame(char *data){
-		FILE *frame = fopen("/dev/shm/cscript.c", "w");
-		if (frame == NULL){
-			perror("Cant build frame");
-			exit(EXIT_FAILURE);
-		}	
-		if (CREATE_MAIN == true){
-				fprintf(frame, "#include<stdio.h>\n"
-					"#include<stdlib.h>\n"
-					"#include<string.h>\n\n\n"
-					"int main(void){\n"
-					"%s\n"
-					"\treturn EXIT_SUCCESS;\n}", data);
-		}
-		else
-				fprintf(frame, "%s", data);
-		fclose(frame);
+void build_frame(char *data)
+{
+	FILE *frame = fopen("/dev/shm/cscript.c", "w");
+	if (frame == NULL)
+	{
+		perror("Cant build frame");
+		exit(EXIT_FAILURE);
+	}
+	if (CREATE_MAIN == true)
+	{
+		fprintf(frame, "#include<stdio.h>\n"
+					   "#include<stdlib.h>\n"
+					   "#include<string.h>\n\n\n"
+					   "int main(void){\n"
+					   "%s\n"
+					   "\treturn EXIT_SUCCESS;\n}",
+				data);
+	}
+	else
+		fprintf(frame, "%s", data);
+	fclose(frame);
 }
 
 // Runs the given script by copy the conntent from the file to a buffer,
 // pass it to build_frame(), compile and run it.
-void run_script(char *script){
-		char *buffer = NULL;
-		int position = 0;
-		FILE *data = fopen(script, "r");
-		if (data == NULL){
-			perror("Can't read from given cscript");
-			exit(EXIT_FAILURE);
-		}
-		fseek(data, 0, SEEK_END);
-		position = ftell(data);
-		buffer = malloc(sizeof(char) * position);
-		if (buffer == NULL){
-			printf("Cant allocate memory to load the cscript. It is too big for your memory!\n");
-			exit(EXIT_FAILURE);
-		}
-		rewind(data);
-		fread(buffer, 1 , position, data);
-		build_frame(buffer);
-		free(buffer);
-		system(strcat(COMPILER, " -g -o /dev/shm/cscript.elf /dev/shm/cscript.c"));
-		system("/dev/shm/cscript.elf");	
+void run_script(char *script)
+{
+	char *buffer = NULL;
+	int file_size = 0;
+	FILE *data = fopen(script, "r");
+	if (data == NULL)
+	{
+		perror("Can't read from given cscript");
+		exit(EXIT_FAILURE);
+	}
+	fseek(data, 0, SEEK_END);
+	file_size = ftell(data);
+	buffer = malloc(sizeof(char) * file_size);
+	if (buffer == NULL)
+	{
+		printf("Cant allocate memory to load the cscript. It is too big for your memory!\n");
+		exit(EXIT_FAILURE);
+	}
+	rewind(data);
+	fread(buffer, 1, file_size, data);
+	build_frame(buffer);
+	free(buffer);
+	system(strcat(COMPILER, " -g -o /dev/shm/cscript.elf /dev/shm/cscript.c"));
+	system("/dev/shm/cscript.elf");
 }
 
 // Test if file exist, if not return 1, else return 0.
-int test_file(){
-		FILE *file = fopen("/dev/shm/cscript.c", "r");
-		if (file == NULL)
-				return 1;
-		else
-				fclose(file);
-		return 0;
+int test_file()
+{
+	FILE *file = fopen("/dev/shm/cscript.c", "r");
+	if (file == NULL)
+		return 1;
+	else
+		fclose(file);
+	return 0;
 }
- void debug(){
-		int test = test_file();
-		if (test){
-				printf("There is nothing to debug...\n");
-				exit(EXIT_FAILURE);
-		}
-		else
-				system(strcat(DEBUGGER, " /dev/shm/cscript.elf"));
+void debug()
+{
+	int test = test_file();
+	if (test)
+	{
+		printf("There is nothing to debug...\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+		system(strcat(DEBUGGER, " /dev/shm/cscript.elf"));
+}
+
+// Save last executed script as compiled executable
+int save_data()
+{
+	char *buffer = NULL;
+	unsigned long int file_size = 0;
+	FILE *elf = fopen("/dev/shm/cscript.elf", "rb");
+	FILE *dest = fopen("executable_cscript.elf", "wb");
+	if (elf == NULL || dest == NULL)
+	{
+		perror("Save Executable");
+		return 1;
+	}
+	fseek(elf, 0, SEEK_END);
+	file_size = ftell(elf);
+		rewind(elf);
+	buffer = malloc(file_size);
+	if ( buffer == NULL)
+	{
+		perror("Copy Executable");
+		return 1;
+	};
+	while ((fread(buffer, 1, sizeof(buffer), elf)) != 0){
+		fwrite(buffer, 1, sizeof(buffer), dest);
+	}
+	fclose(elf);
+	fclose(dest);
+	free(buffer);
+
+	return 0;
 }
